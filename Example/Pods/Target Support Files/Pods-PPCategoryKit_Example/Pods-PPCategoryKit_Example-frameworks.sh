98467f5912b4e194e6d264e1b13647383ed6ac85
郝鹏飞 <haopf@rgnet.cn>
2020-07-17 16:02:40 +0800

    导入第三方库

--- Example/Pods/Target Support Files/Pods-PPCategoryKit_Example/Pods-PPCategoryKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-PPCategoryKit_Example/Pods-PPCategoryKit_Example-frameworks.sh
@@ -161,10 +161,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PPCategoryKit/PPCategoryKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PPCategoryKit/PPCategoryKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
