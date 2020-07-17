849c5de95eb1b838e8957e3647122bdbe95f15c4
郝鹏飞 <haopf@rgnet.cn>
2020-07-17 13:51:21 +0800

    Initial commit

---
+++ Example/PPCategoryKit/PPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PPAppDelegate.h
+//  PPCategoryKit
+//
+//  Created by haopf@rgnet.cn on 07/17/2020.
+//  Copyright (c) 2020 haopf@rgnet.cn. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/PPCategoryKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PPCategoryKit
+//
+//  Created by haopf@rgnet.cn on 07/17/2020.
+//  Copyright (c) 2020 haopf@rgnet.cn. All rights reserved.
+//
+
+@import UIKit;
+#import "PPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PPAppDelegate class]));
+    }
+}
